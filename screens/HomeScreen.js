7f3860c422b2ba0e755b427e690ff957ac55ccce
JaiIrkal <jaivirkal@gmail.com>
2024-07-03 19:58:41 +0530

    Frontend part of search and options completed

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -6,13 +6,18 @@ import { Avatar } from 'react-native-paper'
 const HomeScreen = () => {
 
     const [showSearch, toggleSearch] = useState(false)
+    const [location, setLocation] = useState([1, 2, 3])
+
+    const handleLocation = (loc) => {
+        console.log(loc)
+    }
 
   return (
     <View style={styles.mainContainer}>
       <View style={styles.searchContainer}>
         {
             showSearch?<TextInput placeholder='Search City' style={styles.searchTf}/>:
-            null
+            <Text style={styles.headerTxt}>WeatherWhiz</Text>
         }
         <TouchableOpacity 
             activeOpacity={0.6} 
@@ -29,6 +34,33 @@ const HomeScreen = () => {
             />
         </TouchableOpacity>
       </View>
+        {
+            location.length>0 && showSearch?(
+                <View style={styles.optionContainer}>
+                    {
+                        location.map((loc, index) => (
+                            <TouchableOpacity 
+                                style={styles.option} 
+                                activeOpacity={0.7}
+                                key={index}
+                                onPress={()=> handleLocation(loc)}
+                            >
+                                <Avatar.Icon 
+                                    icon={'map-marker'} 
+                                    size={50} 
+                                    style={{backgroundColor:'transparent'}}
+                                    color='black'
+                                />
+                                <Text style={{
+                                    fontWeight:'700',
+                                    fontSize:18
+                                }}>London, UK</Text>
+                            </TouchableOpacity>
+                        ))
+                    }
+                </View>
+            ):null
+        }
     </View>
   )
 }
@@ -36,6 +68,26 @@ const HomeScreen = () => {
 export default HomeScreen
 
 const styles = StyleSheet.create({
+    option:{
+        backgroundColor:colors.lightGrey,
+        margin:2,
+        padding:8,
+        borderRadius:10,
+        flexDirection:'row',
+        alignItems:'center'
+    },
+    optionContainer:{
+        width:'86%',
+        position:'absolute',
+        top:96,
+        left:22,
+        flexDirection:'column',
+    },
+    headerTxt:{
+        fontSize:35,
+        fontWeight:'700',
+        color:'white'
+    },
     searchIconContainer:{
         justifyContent:'center',
         backgroundColor:colors.yellow,
